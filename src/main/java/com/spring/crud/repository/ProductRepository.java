40614df1c21182844a1f54e630006d0d9f6fb97b
Aclaputra <muhamadacla22@gmail.com>
2022-07-31 18:31:11 +0700

    feat: added ProductRepository

--- src/main/java/com/spring/crud/repository/ProductRepository.java
+++ src/main/java/com/spring/crud/repository/ProductRepository.java
@@ -6,7 +6,11 @@ import com.spring.crud.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    /**
+     * make new method findByName from ProductService
+     * @param name
+     * @return
+     */
     Product findByName(String name);
-    
     
 }
