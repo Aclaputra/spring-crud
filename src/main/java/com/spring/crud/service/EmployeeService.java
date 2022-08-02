7a8210165ea805b3e4dae052b7cbbd9a7e6ab136
Aclaputra <muhamadacla22@gmail.com>
2022-08-03 05:33:51 +0700

    feat: modified employeeservice

--- src/main/java/com/spring/crud/service/EmployeeService.java
+++ src/main/java/com/spring/crud/service/EmployeeService.java
@@ -10,14 +10,53 @@ import com.spring.crud.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-    
+    /**
+     * 
+     */
     @Autowired
     private EmployeeRepository repository;
 
+    /**
+     * 
+     * @param employee
+     * @return
+     */
     public Employee saveEmployee(Employee employee) {
         return repository.save(employee);
     }
+    /**
+     * 
+     * @param employee
+     * @return
+     */
     public List<Employee> saveEmployees(List<Employee> employee) {
         return repository.saveAll(employee);
+    }
+    public List<Employee> getEmployees() {
+        return repository.findAll();
+    }
+    public Employee getEmployeeById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    /**
+     * findByName not listed in library we have to make custom
+     * by making new method in ProductRepository
+     * @param name
+     * @return
+     */
+    public Employee getEmployeeByName(String name) {
+        return repository.findByName(name);
+    }
+    public String deleteEmployee(int id) {
+        repository.deleteById(id);
+        return "employee removed !! " + id;
+    }
+    public Employee updateEmployee(Employee employee) {
+        Employee existingEmpoyee = repository.findById(employee.getId()).orElse(null);
+        existingEmpoyee.setName(employee.getName());
+        existingEmpoyee.setPosition(employee.getPosition());
+        existingEmpoyee.setSalary(employee.getSalary());
+        return repository.save(existingEmpoyee);
     }
 }
